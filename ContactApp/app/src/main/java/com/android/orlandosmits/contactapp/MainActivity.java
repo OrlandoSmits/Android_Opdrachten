98d819be8e29fad21284f0763596ae10d40d15d4
Orlando Smits <orlandosmits@live.nl>
2016-10-28 20:46:43 +0200

    Fixed ActionBar

--- ContactApp/app/src/main/java/com/android/orlandosmits/contactapp/MainActivity.java
+++ ContactApp/app/src/main/java/com/android/orlandosmits/contactapp/MainActivity.java
@@ -5,7 +5,10 @@ import android.database.Cursor;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -36,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         dbHandler = new DatabaseHandler(this);
         mUserList = dbHandler.getUsers();
@@ -60,6 +65,31 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         mUserListView.setOnItemClickListener(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_addPerson:
+                GETRequest();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
